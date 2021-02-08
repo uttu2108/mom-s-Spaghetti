bff3777c2b680d4893ee3d03f9d4a525c759d12d
Utkarsh Srivastav <71812037+UtkarshSrivastav2108@users.noreply.github.com>
2021-02-08 13:52:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,146 @@
+
+
+
+$("#1o1").mouseenter(function() {
+    $("#botton-s").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#botton-s").hide();
+});
+
+$("#1o1").mouseenter(function() {
+    $("#fa-random").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#fa-random").hide();
+});
+
+
+
+// ---------------------------2----------------
+
+
+$("#1o11").mouseenter(function() {
+    $("#botton-s-2").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#botton-s-2").hide();
+});
+
+$("#1o11").mouseenter(function() {
+    $("#fa-random-2").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#fa-random-2").hide();
+});
+
+
+
+// // ---------------------------3----------------
+
+$("#third").mouseenter(function() {
+    $("#botton-s-3").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#botton-s-3").hide();
+});
+
+$("#third").mouseenter(function() {
+    $("#fa-random-3").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#fa-random-3").hide();
+});
+
+// // ---------------------------4----------------
+
+
+$("#fourth").mouseenter(function() {
+    $("#botton-s-4").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#botton-s-4").hide();
+});
+
+$("#fourth").mouseenter(function() {
+    $("#fa-random-4").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#fa-random-4").hide();
+});
+
+
+
+
+// // ---------------------------5----------------
+
+
+$("#five").mouseenter(function() {
+    $("#botton-s-5").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#botton-s-5").hide();
+});
+
+$("#five").mouseenter(function() {
+    $("#fa-random-5").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#fa-random-5").hide();
+});
+
+
+
+// // ---------------------------6----------------
+
+$("#six").mouseenter(function() {
+    $("#botton-s-6").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#botton-s-6").hide();
+});
+
+$("#six").mouseenter(function() {
+    $("#fa-random-6").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#fa-random-6").hide();
+});
+
+
+
+
+// // ---------------------------7----------------
+
+
+$("#seven").mouseenter(function() {
+    $("#botton-s-7").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#botton-s-7").hide();
+});
+
+$("#seven").mouseenter(function() {
+    $("#fa-random-7").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#fa-random-7").hide();
+});
+
+
+
+
+// // ---------------------------8----------------
+
+$("#eight").mouseenter(function() {
+    $("#botton-s-8").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#botton-s-8").hide();
+});
+
+$("#eight").mouseenter(function() {
+    $("#fa-random-8").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#fa-random-8").hide();
+});
+
+
+// // ---------------------------9----------------
+
+$("#nine").mouseenter(function() {
+    $("#botton-s-9").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#botton-s-9").hide();
+});
+
+$("#nine").mouseenter(function() {
+    $("#fa-random-9").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#fa-random-9").hide();
+});
