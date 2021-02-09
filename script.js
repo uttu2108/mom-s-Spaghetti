4b8ed09aeb8d40dc740856d4e6daaec65e9a08e9
Utkarsh Srivastav <71812037+UtkarshSrivastav2108@users.noreply.github.com>
2021-02-09 18:09:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,158 @@
-function openNav(){document.getElementById("mySidenav").style.width="350px"}function closeNav(){document.getElementById("mySidenav").style.width="0"}$("#1o1").mouseenter(function(){$("#botton-s").show().slideUp().slideDown()}).mouseleave(function(){$("#botton-s").slideUp()}),$("#1o1").mouseenter(function(){$("#fa-random").show().slideUp().slideDown()}).mouseleave(function(){$("#fa-random").slideUp()}),$("#1o11").mouseenter(function(){$("#botton-s-2").show().slideUp().slideDown()}).mouseleave(function(){$("#botton-s-2").slideUp()}),$("#1o11").mouseenter(function(){$("#fa-random-2").show().slideUp().slideDown()}).mouseleave(function(){$("#fa-random-2").slideUp()}),$("#third").mouseenter(function(){$("#botton-s-3").show().slideUp().slideDown()}).mouseleave(function(){$("#botton-s-3").slideUp()}),$("#third").mouseenter(function(){$("#fa-random-3").show().slideUp().slideDown()}).mouseleave(function(){$("#fa-random-3").slideUp()}),$("#fourth").mouseenter(function(){$("#botton-s-4").show().slideUp().slideDown()}).mouseleave(function(){$("#botton-s-4").slideUp()}),$("#fourth").mouseenter(function(){$("#fa-random-4").show().slideUp().slideDown()}).mouseleave(function(){$("#fa-random-4").slideUp()}),$("#five").mouseenter(function(){$("#botton-s-5").show().slideUp().slideDown()}).mouseleave(function(){$("#botton-s-5").slideUp()}),$("#five").mouseenter(function(){$("#fa-random-5").show().slideUp().slideDown()}).mouseleave(function(){$("#fa-random-5").slideUp()}),$("#six").mouseenter(function(){$("#botton-s-6").show().slideUp().slideDown()}).mouseleave(function(){$("#botton-s-6").slideUp()}),$("#six").mouseenter(function(){$("#fa-random-6").show().slideUp().slideDown()}).mouseleave(function(){$("#fa-random-6").slideUp()}),$("#seven").mouseenter(function(){$("#botton-s-7").show().slideUp().slideDown()}).mouseleave(function(){$("#botton-s-7").slideUp()}),$("#seven").mouseenter(function(){$("#fa-random-7").show().slideUp().slideDown()}).mouseleave(function(){$("#fa-random-7").slideUp()}),$("#eight").mouseenter(function(){$("#botton-s-8").show().slideUp().slideDown()}).mouseleave(function(){$("#botton-s-8").slideUp()}),$("#eight").mouseenter(function(){$("#fa-random-8").show().slideUp().slideDown()}).mouseleave(function(){$("#fa-random-8").slideUp()}),$("#nine").mouseenter(function(){$("#botton-s-9").show().slideUp().slideDown()}).mouseleave(function(){$("#botton-s-9").slideUp()}),$("#nine").mouseenter(function(){$("#fa-random-9").show().slideUp().slideDown()}).mouseleave(function(){$("#fa-random-9").slideUp()});
+
+
+
+$("#1o1").mouseenter(function() {
+    $("#botton-s").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#botton-s").slideUp();
+});
+
+$("#1o1").mouseenter(function() {
+    $("#fa-random").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#fa-random").slideUp();
+});
+
+
+
+// ---------------------------2----------------
+
+
+$("#1o11").mouseenter(function() {
+    $("#botton-s-2").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#botton-s-2").slideUp();
+});
+
+$("#1o11").mouseenter(function() {
+    $("#fa-random-2").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#fa-random-2").slideUp();
+});
+
+
+
+// // ---------------------------3----------------
+
+$("#third").mouseenter(function() {
+    $("#botton-s-3").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#botton-s-3").slideUp();
+});
+
+$("#third").mouseenter(function() {
+    $("#fa-random-3").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#fa-random-3").slideUp();
+});
+
+// // ---------------------------4----------------
+
+
+$("#fourth").mouseenter(function() {
+    $("#botton-s-4").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#botton-s-4").slideUp();
+});
+
+$("#fourth").mouseenter(function() {
+    $("#fa-random-4").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#fa-random-4").slideUp();
+});
+
+
+
+
+// // ---------------------------5----------------
+
+
+$("#five").mouseenter(function() {
+    $("#botton-s-5").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#botton-s-5").slideUp();
+});
+
+$("#five").mouseenter(function() {
+    $("#fa-random-5").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#fa-random-5").slideUp();
+});
+
+
+
+// // ---------------------------6----------------
+
+$("#six").mouseenter(function() {
+    $("#botton-s-6").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#botton-s-6").slideUp();
+});
+
+$("#six").mouseenter(function() {
+    $("#fa-random-6").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#fa-random-6").slideUp();
+});
+
+
+
+
+// // ---------------------------7----------------
+
+
+$("#seven").mouseenter(function() {
+    $("#botton-s-7").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#botton-s-7").slideUp();
+});
+
+$("#seven").mouseenter(function() {
+    $("#fa-random-7").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#fa-random-7").slideUp();
+});
+
+
+
+
+// // ---------------------------8----------------
+
+$("#eight").mouseenter(function() {
+    $("#botton-s-8").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#botton-s-8").slideUp();
+});
+
+$("#eight").mouseenter(function() {
+    $("#fa-random-8").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#fa-random-8").slideUp();
+});
+
+
+// // ---------------------------9----------------
+
+$("#nine").mouseenter(function() {
+    $("#botton-s-9").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#botton-s-9").slideUp();
+});
+
+$("#nine").mouseenter(function() {
+    $("#fa-random-9").show().slideUp().slideDown();
+}).mouseleave(function() {
+    $("#fa-random-9").slideUp();
+});
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "350px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+
+
